c4095c66922d427062d79c5ca721bc4f9c5a0889
juls <julianmacmang@gmail.com>
2020-03-26 21:35:47 +0800

    updates config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
+    title: `Blog`,
+    author: `Julian Macmang`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://blog.jvlivn.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `MVCMVNG`,
     },
   },
   plugins: [
@@ -64,7 +64,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+         trackingId: `UA-26596615-2`,
       },
     },
     {
